ac51ca14a35feca2b76c436875c58b654bf37061
ahmedshams99 <46957449+ahmedshams99@users.noreply.github.com>
2019-04-04 00:03:24 +0200

    Update Clients.js

--- src/Components/Clients.js
+++ src/Components/Clients.js
@@ -9,17 +9,15 @@ export default class Clients extends Component{
         
         return(
             <div id = "Clients" className="Clients">
-                <img alt="" src="http://www.egyptfoodsgroup.com/img/logo.png"/>
-                <img alt="" src="https://www.elmotaheda-web.com/images/cinnabon.png"/>
-                <img alt="" src="https://upload.wikimedia.org/wikipedia/en/thumb/1/12/Egyptian_Premier_League_logo.png/200px-Egyptian_Premier_League_logo.png"/>
+                <img alt="" src="http://nissannews.com/en-US/nissan/usa/photos/e05c/efce/e05cefce-ebf8-466a-95e0-ebd1ea503f6a-768x432.jpg"/>
+                <img alt="" src="https://www.motogp.com.au/sites/default/files/styles/main_visual_element_transp_image_default/public/partner/image/logo/FOR-SPONSOR-Suzuki-Logo.png"/>
+                <img alt="" src="https://static.wixstatic.com/media/11309f_50b1114c68a14a5081e269fd3ad3b7b1~mv2.png/v1/fill/w_288,h_66,al_c,q_80,usm_0.66_1.00_0.01/modern%20motors%20logo.webp"/>
+                <img alt="" src="https://static.wixstatic.com/media/11309f_557b3be5248846fa822d48477d00538f~mv2.png/v1/fill/w_229,h_52,al_c,q_80,usm_0.66_1.00_0.01/gevodan-0%D9%A1.webp"/>
                 <br/>
-                <img alt="" src="https://ymcss.b8cdn.com/assets/yallamotor_large.png"/>
-                <img alt="" src="https://upload.wikimedia.org/wikipedia/en/thumb/e/e8/Shell_logo.svg/1200px-Shell_logo.svg.png"/>
-                <img alt="" src="http://pluspng.com/img-png/samsung-logo-png-exciting-samsung-logo-vector-free-download-58-for-design-logo-with-samsung-logo-vector-free-download-1269.png"/>
-                <br/>
-                <img alt="" src="https://yt3.ggpht.com/a-/AAuE7mCNFdey-43t_bfFWi4makS7MyR8LnBMBeFCxQ=s900-mo-c-c0xffffffff-rj-k-no"/>
-                <img alt="" src="http://katameyadowntown.com/wp-content/uploads/2016/09/vodafone-1.png"/>
-                <img alt="" src="https://www.orange.eg/Style%20Library/OrangeImages/OrangeLogo-fb.jpg"/>
+                <img alt="" src="https://static.wixstatic.com/media/11309f_3dc1ed44981f4ac3b3480d88edf491c2~mv2.png/v1/fill/w_229,h_187,al_c,q_80,usm_0.66_1.00_0.01/art_square_logo-0%D9%A1.webp"/>
+                <img alt="" src="https://static.wixstatic.com/media/11309f_a55d794e366243dd80d8ade33249a823~mv2_d_2737_2641_s_4_2.png/v1/fill/w_216,h_209,al_c,q_80,usm_0.66_1.00_0.01/stash-0%D9%A1.webp"/>
+                <img alt="" src="https://static.wixstatic.com/media/11309f_153a28f517984a868e89ee1756c543e9~mv2_d_2970_1307_s_2.png/v1/fill/w_229,h_101,al_c,q_80,usm_0.66_1.00_0.01/canadian%20council-0%D9%A1.webp"/>
+                <img alt="" src="https://static.wixstatic.com/media/11309f_f23ee79af37340b1b8dcd1b491384b28~mv2.jpg/v1/fill/w_229,h_99,al_c,q_80,usm_0.66_1.00_0.01/ta2ssima.webp"/>
             </div>
         )
     }
