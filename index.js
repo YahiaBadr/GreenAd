be2bb40cc379252116b1b0813bb54fb4f58aa069
yahiabadr <yahya.hisham97@gmail.com>
2019-05-01 16:22:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,18 @@ exports.api = functions.https.onRequest(app)
 // Use it with post
 app.use(express.json())
 app.use(cors());
-app.use(express.static('client'))
 // Default route (entry point)
-app.get('/', (req, res) => {
-    res.send(`<h1>Welcome</h1>`)
-})
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    //Homepage
+    app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")));
+  } else {
+    app.get("/", (req, res) => res.send("Homepage"));
+  }
+// app.get('/', (req, res) => {
+    // res.send(`<h1>Welcome</h1>`)
+// })
 
 app.post('/api/sendMail',sendMail.sendMail);
 
