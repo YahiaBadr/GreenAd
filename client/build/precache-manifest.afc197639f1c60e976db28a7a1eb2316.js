d5f763c16c8d3a6b6a90eb409ad2b33728cd3943
Yahia Badr <30608188+YahiaBadr@users.noreply.github.com>
2019-05-01 16:56:17 +0200

    add build folder

---
+++ client/build/precache-manifest.afc197639f1c60e976db28a7a1eb2316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c16683f2f9d2afc01148",
+    "url": "/static/js/main.56eebfbf.chunk.js"
+  },
+  {
+    "revision": "b0f535bd3bba66f856d0",
+    "url": "/static/js/2.b9e1e613.chunk.js"
+  },
+  {
+    "revision": "c16683f2f9d2afc01148",
+    "url": "/static/css/main.a37fd918.chunk.css"
+  },
+  {
+    "revision": "b0f535bd3bba66f856d0",
+    "url": "/static/css/2.08451967.chunk.css"
+  },
+  {
+    "revision": "dcf5ef2696cd3fadcb7a322e36dd9982",
+    "url": "/index.html"
+  }
+];
