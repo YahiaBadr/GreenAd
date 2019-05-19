b34d5d1f821e4a5e8dc3dde7223e1489047ad99a
yahiabadr <yahya.hisham97@gmail.com>
2019-05-19 22:50:41 +0200

    remove map files

---
+++ client/build/precache-manifest.68cf8ac6e2fcf8b20f66c42412ea350b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8479686b0be18308c47c",
+    "url": "/static/js/main.f8849d2a.chunk.js"
+  },
+  {
+    "revision": "b0f535bd3bba66f856d0",
+    "url": "/static/js/2.b9e1e613.chunk.js"
+  },
+  {
+    "revision": "8479686b0be18308c47c",
+    "url": "/static/css/main.a37fd918.chunk.css"
+  },
+  {
+    "revision": "b0f535bd3bba66f856d0",
+    "url": "/static/css/2.08451967.chunk.css"
+  },
+  {
+    "revision": "164aa5e9f6da34d0aa82b4a6f3dc23be",
+    "url": "/index.html"
+  }
+];
